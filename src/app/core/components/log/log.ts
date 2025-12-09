45bb8a1e96275f0e1c4acfd3e6d0f3ea186ece59
Robert Porteles <robertporteless@gmail.com>
2025-12-09 17:50:50 -0300

    acertando scss

--- src/app/core/components/log/log.ts
+++ src/app/core/components/log/log.ts
@@ -2,6 +2,9 @@ import { CommonModule } from '@angular/common';
 import { AfterViewInit, Component, inject, OnInit, ViewChild } from '@angular/core';
 import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
 import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 import { Funcionario } from '../../models/funcionario';
 import { Localizacao } from '../../models/localizacao';
 import { PontoService } from '../../services/ponto-service';
@@ -9,12 +12,12 @@ import { Ponto } from '../../models/ponto';
 
 @Component({
   selector: 'app-log',
-  imports: [CommonModule, MatPaginatorModule, MatTableModule],
+  imports: [CommonModule, MatPaginatorModule, MatTableModule, MatCardModule, MatIconModule, MatButtonModule],
   templateUrl: './log.html',
   styleUrl: './log.css',
 })
-export class Log implements OnInit, AfterViewInit{
-  
+export class Log implements OnInit, AfterViewInit {
+
 
   private service = inject(PontoService)
 
@@ -24,9 +27,9 @@ export class Log implements OnInit, AfterViewInit{
   dataSourceLocalizacao = new MatTableDataSource<Localizacao>()
   dataSourcePontos = new MatTableDataSource<Ponto>()
 
-  displayColumnFuncionario = ["id", "nome","email","cargo"]
+  displayColumnFuncionario = ["id", "nome", "email", "cargo"]
   displayColumnLocalizacao = ["id", "latitude", "longitude", "descricao", "ponto_id"]
-  displayColumnPonto       = ["id", "descricao", "data", "hora", "funcionarios_id"]
+  displayColumnPonto = ["id", "descricao", "data", "hora", "funcionarios_id"]
 
 
   @ViewChild("Funcionario") funcionario!: MatPaginator;
@@ -35,32 +38,32 @@ export class Log implements OnInit, AfterViewInit{
 
 
 
-  ngOnInit(){
+  ngOnInit() {
     this.getAllFuncionarios();
     this.getAllPonto();
     this.getAllLocalizacao();
-    
+
   }
-  ngAfterViewInit(){
+  ngAfterViewInit() {
     this.dataSourceFuncionario.paginator = this.funcionario;
     this.dataSourcePontos.paginator = this.pontos;
     this.dataSourceLocalizacao.paginator = this.localizacao
   }
 
-  getAllFuncionarios(){
-     this.service.getAllFuncionario().subscribe((funcionario)=>{
-        this.dataSourceFuncionario.data =  funcionario;
-     })
+  getAllFuncionarios() {
+    this.service.getAllFuncionario().subscribe((funcionario) => {
+      this.dataSourceFuncionario.data = funcionario;
+    })
   }
-  getAllLocalizacao(){
-     this.service.getAllLocalizacao().subscribe((localizacao)=>{
+  getAllLocalizacao() {
+    this.service.getAllLocalizacao().subscribe((localizacao) => {
       this.dataSourceLocalizacao.data = localizacao;
-     })
+    })
   }
-  getAllPonto(){
-     this.service.getAllPonto().subscribe((ponto)=>{
+  getAllPonto() {
+    this.service.getAllPonto().subscribe((ponto) => {
       this.dataSourcePontos.data = ponto;
-     })
+    })
   }
 
 
